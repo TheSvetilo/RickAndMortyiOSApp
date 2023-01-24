bf1e12febc972c58924996b59597050cd3ff562e
Vlad <svet2315@gmail.com>
2023-01-24 22:18:43 +0300

    Initial commit

---
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RMEpisodeViewController.swift
+//  RickAndMorty
+//
+//  Created by Bogdanov, Vladislav on 24.01.2023.
+//
+
+import UIKit
+
+final class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Episodes"
+    }
+}
---
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RMLocationViewController.swift
+//  RickAndMorty
+//
+//  Created by Bogdanov, Vladislav on 24.01.2023.
+//
+
+import UIKit
+
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Locations"
+    }
+}
